fdfc7b627293db3b74db3357203a51a0d8cd07e5
Mario Parra <anzurio@gmail.com>
2019-08-07 11:31:11 -0500

    Add simple file output

---
+++ AdventOfCode2018/FileOutput.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode2018
+{
+    public class FileOutput : OutputStream
+    {
+        private string Filename { get; set; }
+        private FileStream FileStream { get; set; }
+        private StreamWriter Writer { get; set; }
+
+        public FileOutput(string filename)
+        {
+            Filename = filename;
+        }
+
+        public override void Dispose()
+        {
+            if (FileStream != null)
+            {
+                Writer.Flush();
+                FileStream.Flush();
+                Writer.Close();
+                FileStream.Close();
+                Writer.Dispose();
+                FileStream.Dispose();
+            }
+        }
+
+        public override OutputStream Open()
+        {
+            FileStream = File.Create(Filename);
+            Writer = new StreamWriter(FileStream);
+            return this;
+        }
+
+        public override void Write(string s)
+        {
+            Writer.Write(s);
+        }
+
+        public override void Write(char c)
+        {
+            Writer.Write(c);
+        }
+
+        public override void WriteNewLine()
+        {
+            Writer.WriteLine();
+        }
+    }
+}

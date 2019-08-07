6d04c5c79800610fa179a91ac54c15b31034500f
Mario Parra <anzurio@gmail.com>
2019-08-07 13:17:15 -0500

    Run the actual Day 10 challenge

--- AdventOfCode2018/Program.cs
+++ AdventOfCode2018/Program.cs
@@ -6,7 +6,12 @@ namespace AdventOfCode2018
     {
         static void Main(string[] args)
         {
+            var problem10 = new Day10.TheStarsAlign();
+            problem10.ParseInput(SampleInputs.TheStarsAlign);
+            problem10.Solve(new FileOutput("day10.txt"));
+
             Console.WriteLine("Hello World!");
+            Console.Read();
         }
     }
 }

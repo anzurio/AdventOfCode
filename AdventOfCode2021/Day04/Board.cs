f72e845e2b1c6900bfc744113d391daa42e32f4e
anzurio <anzurio@gmail.com>
2021-12-05 16:34:47 -0600

    Day 4 part 1

---
+++ AdventOfCode2021/Day04/Board.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Day04
+{
+    class Board
+    {
+        HashSet<int>[] WinningSets = new HashSet<int>[10];
+        HashSet<int> availableValues = new HashSet<int>();
+
+        public int WinningTurn { get; private set; }
+        public int Score { get; private set; }
+
+        public Board(int[] drawings, int[][] board)
+        {
+            WinningTurn = drawings.Length;
+            WinningSets[5] = new HashSet<int>();
+            WinningSets[6] = new HashSet<int>();
+            WinningSets[7] = new HashSet<int>();
+            WinningSets[8] = new HashSet<int>();
+            WinningSets[9] = new HashSet<int>();
+            for (int i = 0; i < board.Length; i++)
+            {
+                WinningSets[i] = board[i].ToHashSet();
+                for (int j = 0; j < board[i].Length; j++)
+                {
+                    availableValues.Add(board[i][j]);
+                    WinningSets[5 + j].Add(board[i][j]);
+                }
+            }
+
+            for (var turn = 0; turn < drawings.Length; turn++)
+            {
+                availableValues.Remove(drawings[turn]);
+                foreach (var set in WinningSets)
+                {
+                    set.Remove(drawings[turn]);
+                }
+                if (WinningSets.Any(x => x.Count == 0))
+                {
+                    Score = availableValues.Sum() * drawings[turn];
+                    WinningTurn = turn;
+                    break;
+                }
+               
+            }
+
+        }
+    }
+}
---
+++ AdventOfCode2021/Day04/GiantSquid.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2021.Day04
+{
+    public class GiantSquid : ISolver
+    {
+        private int[] drawings;
+        private List<Board> boards = new List<Board>();
+
+        public void ParseInput(IEnumerable<string> input)
+        {
+            var array = input.ToArray();
+            
+            drawings = array[0].Split(",").Select(x => int.Parse(x)).ToArray();
+            for (int i = 2; i < input.Count(); i += 6)
+            {
+                var board = new int[5][];
+                for (int j = 0; j < 5; j++)
+                {
+                    board[j] = array[i + j].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+                    
+                }
+                boards.Add(new Board(drawings, board));
+            }
+        }
+
+        public void Solve(OutputStream outputStream)
+        {
+            var winningBoard = boards.OrderBy(x => x.WinningTurn).First();
+            outputStream.Write(winningBoard.Score);
+            outputStream.WriteNewLine();
+        }
+    }
+}
